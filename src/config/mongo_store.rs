c2b354c1833f8b079d7baab90b839d85aad6840c
integ <integ@local>
2026-07-18 04:14:08 -0400

    Use implicit mutable transaction binding

--- src/config/mongo_store.rs
+++ src/config/mongo_store.rs
@@ -9316,7 +9316,7 @@ mod inner {
                             connection,
                             prepared_docs,
                             guard_params,
-                            ref mut upsert_changes,
+                            upsert_changes,
                             namespace,
                             restored_proxy_id,
                             validation_http_client,
