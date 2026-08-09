0b19ab49a4c7fe1205b6deece43fca0b8bf262ec
integ <integ@local>
2026-08-09 08:38:01 -0400

    Apply hosted telemetry test formatting

--- tests/unit/plugins/mesh_telemetry_tag_cel_tests.rs
+++ tests/unit/plugins/mesh_telemetry_tag_cel_tests.rs
@@ -231,10 +231,7 @@ fn rejects_unsupported_malformed_and_costly_cel() {
     assert!(err.contains("exceeds maximum length"));
     assert!(!err.contains("xxxxx"));
 
-    let whitespace_padded = format!(
-        "{}request.host",
-        " ".repeat(MAX_METRIC_TAG_CEL_EXPR_LEN)
-    );
+    let whitespace_padded = format!("{}request.host", " ".repeat(MAX_METRIC_TAG_CEL_EXPR_LEN));
     let err = parse_metric_tag_cel_expression(&whitespace_padded)
         .expect_err("raw expression length must include surrounding whitespace");
     assert!(err.contains("exceeds maximum length"));
